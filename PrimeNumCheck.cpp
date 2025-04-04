d762f789dcdb94ab50d6413874193adf5d45fe14
Pradhyum-K <pkaund@uw.edu>
2025-04-03 21:31:09 -0700

    GRADE THIS FILE PROFESSOR!

--- PrimeNumCheck.cpp
+++ PrimeNumCheck.cpp
@@ -14,26 +14,6 @@ int main() {
 }
 
 void isPrime(int num) {
-  /*if (num == 2) {
-    cout << "PASS!" << "\n";
-  }
-
-  if (num <= 1) {
-    cout << "FAIL!" << "\n";
-  }
-
-  else {
-    for (int i = 2; i < sqrt(num); i++) {
-      if (num % i == 0) {
-        cout << "FAIL!" << "\n";
-      }
-
-      else {
-        cout << "PASS!" << "\n";
-      }
-    }
-  }*/
-
   if (num == 2) {
     cout << "PASS!" << "\n";
   }
@@ -58,13 +38,13 @@ void isPrime(int num) {
 void test() {
 
   // 2: PASS
-  // 13: PASS
+  // 7: PASS
   // 15: FAIL
   // 4: FAIL
   // PASS means the number is prime and FAIL means the number isn't
 
   isPrime(2);
-  isPrime(13);
+  isPrime(17);
   isPrime(15);
   isPrime(4);
 
