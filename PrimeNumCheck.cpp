c72c54069cb74b01616dda9b514534cef018b3ce
Pradhyum-K <pkaund@uw.edu>
2025-04-03 19:53:42 -0700

    Need to finish-Currently Working

--- PrimeNumCheck.cpp
+++ PrimeNumCheck.cpp
@@ -39,7 +39,7 @@ void test() {
 
   // 2: PASS
   // 3: PASS
-  // 5: PASS
+  // 7: PASS
   // 1: FAIL
   // 4: FAIL
   // 8: FAIL
@@ -47,7 +47,7 @@ void test() {
 
   cout << isPrime(2);
   cout << isPrime(3);
-  cout << isPrime(5);
+  cout << isPrime(7);
   cout << isPrime(1);
   cout << isPrime(4);
   cout << isPrime(8);
